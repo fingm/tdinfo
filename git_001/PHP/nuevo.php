2c483c404bbdec09995a39ffcf59e0370765d603
fingm <matiascartro@gmail.com>
2022-01-31 21:23:55 -0300

    clase del lunes

---
+++ git_001/PHP/nuevo.php
@@ -0,0 +1,18 @@
+<?php
+    $a=5;
+    $b=10;
+?>
+
+<!DOCTYPE>
+
+<html>
+    <head>
+        <title>Primer paso</title>
+    </head>
+    <body>
+        <?php
+            echo $a + $b,"<br>";
+            echo $a;
+        ?>
+    </body>
+</html>
---
+++ git_001/PHP/variables.php
@@ -0,0 +1,23 @@
+<?php
+    $a = 500;
+    $b = 300;
+?>
+
+
+<!DOCTYPE>
+
+<html>
+    <head>
+
+    </head>    
+
+    <body>  
+      <?php
+        if ( $a>$b ){
+            echo "si";
+        }   
+      ?>  
+
+    </body>
+
+</html>
